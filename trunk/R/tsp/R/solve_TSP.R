86d1e8997b75ac466e82fb6348d7bf9501478b9d
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-08-25 14:55:39 +0000

    added solve_TSP.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsp@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/R/tsp/R/solve_TSP.R
@@ -0,0 +1,33 @@
+solve_TSP <- function(x, method = NULL, ...) {
+
+    # check parameters
+    if(!inherits(x, "TSP")) x <- TSP(x)
+       
+    # methods
+    methods <- c(
+        "farthest_insertion",      # standard
+        "nearest_insertion",
+        "greedy",
+        "concorde"
+    )
+
+    if(is.null(method)) methodNr <- 1
+    else methodNr <- pmatch(tolower(method), tolower(methods))
+    if(is.na(methodNr)) stop (paste("Unknown method:",sQuote(method)))
+
+
+    # work horses
+    if(methodNr == 1) {
+        order <- tsp_insertion(x, nearest = FALSE, ...)
+    }else if(methodNr == 2) {
+        order <- tsp_insertion(x, nearest = TRUE, ...)
+    }else if(methodNr == 3) {
+        order <- tsp_greedy(x, ...)
+    }else if(methodNr == 4) {
+        order <- tsp_concorde(x, ...)
+    }
+
+    class(order) <- c("TSP_tour", class(order))
+    attr(order, "method") <- methods[methodNr]
+    return(order)
+}
