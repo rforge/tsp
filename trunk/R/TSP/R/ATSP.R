ec280efba297e6692763cafe7959af4c54bfafeb
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-10-30 11:12:23 +0000

    work in progress.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsp@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/R/TSP/R/ATSP.R
@@ -0,0 +1,41 @@
+## create a (asymmetric) ATSP problem
+ATSP <- function(x) {
+    if(inherits(x, "ATSP")) return(x)
+    
+    ## check x
+    if(inherits(x, "dist")) {
+        x <- as.matrix(x)
+    }else if(is.matrix(x) && dim(x)[1] == dim(x)[2]) {
+        ## matrix is ok
+    }else stop("ATSP requires a square matrix")
+
+    ## check for NAs
+    if(any(is.nan(x))) stop(paste(sQuote("NAs"), "not supported"))
+    
+    class(x) <- c("ATSP", class(x))
+    attr(x, "method") <- "unknown" 
+    x
+}
+
+## print
+print.ATSP <- function(x, ...) {
+    cat("object of class", sQuote(class(x)[1]), " (asymmetric TSP) \n")
+    cat(n_of_cities(x), "cities", 
+        paste("(distance ", sQuote(attr(x, "method")),")", sep=""), "\n")
+}
+
+
+## number of cities
+n_of_cities.ATSP <- function(x) nrow(x)
+
+## labels
+labels.ATSP <- function(object, ...) dimnames(r)[[1]]
+
+## image
+image.ATSP <- function(x, order, col = gray.colors(64), ...) {
+    p <- n_of_cities(x)
+    if(missing(order)) order <- 1:p
+    
+    image.default(1:p, 1:p, x[order, order], col = col, ...)
+}
+
