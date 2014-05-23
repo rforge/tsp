3b16c26d26649b5dca7870a2bb6e61c9523d6f30
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-23 15:04:19 +0000

    INF and -INF is now handled in solve_TSP.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsp@766 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/solve_TSP.R
+++ pkg/R/solve_TSP.R
@@ -20,61 +20,82 @@
 
 ## TSP
 solve_TSP.TSP <- function(x, method = NULL, control = NULL) {
-    .solve_TSP(x, method, control)
+  .solve_TSP(x, method, control)
 }
 
 ## ATSP
 solve_TSP.ATSP <- function(x, method = NULL, control = NULL) {
-    .solve_TSP(x, method, control)
+  .solve_TSP(x, method, control)
 }
 
 ## generic
 solve_TSP <- function(x, method = NULL, control = NULL)
-    UseMethod("solve_TSP")
-    
+  UseMethod("solve_TSP")
+
 
 ## workhorse
 .solve_TSP <- function(x, method = NULL, control = NULL) {
-       
-    ## methods
-    methods <- c(
-        "nearest_insertion",    ## standard
-        "farthest_insertion",     
-        "cheapest_insertion",     
-        "arbitrary_insertion",     
-        "nn",
-        "repetitive_nn",
-        "2-opt",
-        "concorde",
-        "linkern"
-    )
-
-    if(is.null(method)) methodNr <- 1
-    else methodNr <- pmatch(tolower(method), tolower(methods))
-    if(is.na(methodNr)) stop (paste("Unknown method:",sQuote(method)))
-
-
+  
+  ## methods
+  methods <- c(
+    "nearest_insertion",    ## standard
+    "farthest_insertion",     
+    "cheapest_insertion",     
+    "arbitrary_insertion",     
+    "nn",
+    "repetitive_nn",
+    "2-opt",
+    "concorde",
+    "linkern"
+  )
+  
+  if(is.null(method)) methodNr <- 1
+  else methodNr <- pmatch(tolower(method), tolower(methods))
+  if(is.na(methodNr)) stop (paste("Unknown method:",sQuote(method)))
+  
+  ### concorde fixes INF differently
+  if(methodNr == 8) {
+    order <- tsp_concorde(x, control = control)
+  }else{
+    
+    ### fix INF first
+    if(any(is.finite(x))) {
+      x_pos_inf <- x == Inf
+      x_neg_inf <- x == -Inf
+      
+      max_x <- max(x[!x_pos_inf], na.rm = TRUE) 
+      min_x <- min(x[!x_neg_inf], na.rm = TRUE)
+      range_x <- max_x-min_x
+      
+      if(any(x_pos_inf)) {
+        x[x_pos_inf] <- max_x + range_x
+      }
+      if(any(x_neg_inf)) {
+        x[x_neg_inf] <- min_x - range_x
+      }
+    }
+    
     ## work horses
     if(methodNr == 1) {
-        order <- tsp_insertion(x, type = "nearest", control = control)
+      order <- tsp_insertion(x, type = "nearest", control = control)
     }else if(methodNr == 2) {
-        order <- tsp_insertion(x, type = "farthest", control = control)
+      order <- tsp_insertion(x, type = "farthest", control = control)
     }else if(methodNr == 3) {
-        order <- tsp_insertion(x, type = "cheapest", control = control)
+      order <- tsp_insertion(x, type = "cheapest", control = control)
     }else if(methodNr == 4) {
-        order <- tsp_insertion(x, type = "arbitrary", control = control)
+      order <- tsp_insertion(x, type = "arbitrary", control = control)
     }else if(methodNr == 5) {
-        order <- tsp_nn(x, control = control)
+      order <- tsp_nn(x, control = control)
     }else if(methodNr == 6) {
-        order <- tsp_repetitive_nn(x, control = control)
+      order <- tsp_repetitive_nn(x, control = control)
     }else if(methodNr == 7) {
-        order <- tsp_two_opt(x, control = control)
-    }else if(methodNr == 8) {
-        order <- tsp_concorde(x, control = control)
+      order <- tsp_two_opt(x, control = control)
+      #    }else if(methodNr == 8) {
+      #        order <- tsp_concorde(x, control = control)
     }else if(methodNr == 9) {
-        order <- tsp_linkern(x, control = control)
+      order <- tsp_linkern(x, control = control)
     }
-
-    
-    TOUR(order, method=methods[methodNr], tsp=x)
+  }
+  
+  TOUR(order, method=methods[methodNr], tsp=x)
 }
